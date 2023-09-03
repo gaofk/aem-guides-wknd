0b1f051a7218f40f3a9e6809b2893f698b299319
Fengkui Gao(C) <fengkui.gao@designory.com>
2023-09-03 09:14:32 +0900

    add new function

---
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/services/ComponentsInfoService.java
@@ -0,0 +1,16 @@
+package com.adobe.aem.guides.wknd.core.services;
+
+import java.util.List;
+import java.util.Map;
+
+public interface ComponentsInfoService {
+
+    /**
+     * @param path the base press releases page path, if configured
+     * @return newest press release items
+     */
+    List<Map<String, String>> getComponents(final String path);
+
+    List<Map<String, String>> getComponentReferences(final String component);
+
+}
